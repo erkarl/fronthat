c070fbfbbcf84f616ffdaedf7f8832d8fda21210
Karl Ranna <karl@karlranna.com>
2017-10-07 11:05:15 +0300

    Use native scrollTo instead of jQuery scrollTop

--- app/mixins/scroll-to-top.js
+++ app/mixins/scroll-to-top.js
@@ -1,9 +1,8 @@
-import $ from 'jquery';
 import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
   didInsertElement() {
     this._super(...arguments);
-    $(document).scrollTop(0);
+    window.scrollTo(0, 0);
   }
 });
