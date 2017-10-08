452b4d7bb1b8356ef15ac9083bbd2acce429d968
Kristjan Ranna <kristjanranna@gmail.com>
2017-10-08 16:59:00 +0300

    Add mailing list to main page

--- app/components/first-eye-contact-area/component.js
+++ app/components/first-eye-contact-area/component.js
@@ -7,6 +7,7 @@ export default Component.extend({
   layout: hbs`
     <div class="first-eye-contact-area">
       <h1>REMOTE DEVELOPMENT AND DESIGN JOBS</h1>
+      <p class="mail-paragraph">Want to stay updated with newly posted jobs? Join our <a href="https://www.getdrip.com/forms/64302472/submissions/new" rel="noopener noreferrer" target="_blank">mailing list</a>! </p>
     </div>
   `
 });
