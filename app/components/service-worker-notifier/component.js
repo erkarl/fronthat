4cb16d0964c17a7802a3107b5bc8760415a6db72
Karl Ranna <karl@karlranna.com>
2017-10-26 16:20:06 +0300

    Display notification when new version available

---
+++ app/components/service-worker-notifier/component.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import hbs from 'htmlbars-inline-precompile';
+
+export default Component.extend({
+  layout: hbs`
+    {{#service-worker-update-notify}}
+      <div class="service-worker-update">
+        <a class="service-worker-update-notify" href="">
+          A new version of FrontHAT is available, click here to update.
+        </a>
+      </div>
+    {{/service-worker-update-notify}}
+  `
+});
