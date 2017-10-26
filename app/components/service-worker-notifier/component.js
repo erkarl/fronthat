c0c8ef39de4f3719473856ee7597c42aa5b9f4f8
Karl Ranna <karl@karlranna.com>
2017-10-26 16:35:44 +0300

    Fix service worker update notifier

--- app/components/service-worker-notifier/component.js
+++ app/components/service-worker-notifier/component.js
@@ -4,7 +4,7 @@ import hbs from 'htmlbars-inline-precompile';
 export default Component.extend({
   layout: hbs`
     {{#service-worker-update-notify}}
-      <div class="service-worker-update">
+      <div class="service-worker-notifier">
         <a class="service-worker-update-notify" href="">
           A new version of FrontHAT is available, click here to update.
         </a>
